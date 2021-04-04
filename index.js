f450cbfed889b764cb70b5260a5b82fc7d105106
Rainey <changjiu1391@126.com>
2021-04-04 17:45:30 +0800

    4-4三个文件

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 
+
+
+const btn = document.querySelector('button');
+
+
+
+ btn.addEventListener('click',handler1);
+
+function handler1(e) {
+     e.preventDefault();
+  document.documentElement.classList.add("color-eee");
+  btn.classList.add('insetShadow');
+    alert("you have clicked button ");
+
+
+}
